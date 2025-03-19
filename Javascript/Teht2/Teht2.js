afdbb4cc25e194208cf0a8d1ed67f5d13858b7e9
iirokin <2306039@edu.karelia.fi>
2025-03-19 15:35:04 +0200

    Täydennetty versio

--- Javascript/Teht2/Teht2.js
+++ Javascript/Teht2/Teht2.js
@@ -1,31 +1,40 @@
 // Puhelinluettelon luonti
-let Phonenumber_Directory = [{
+let Phonenumber_Directory = [
+  {
     Name: "Pekka",
-    PhoneNumber: 1234512345
-}, {
+    PhoneNumber: 1234512345,
+  },
+  {
     Name: "Miika",
-    PhoneNumber: 1234567890
-}];
+    PhoneNumber: 1234567890,
+  },
+];
 
 let reader = require("readline-sync");
 let inputName = reader.question("Syötä Nimi: ");
 let inputNumber = reader.question("Syötä Puhelin numero: ");
 
-// let newEntry = {Name: inputName, PhoneNumber: inputNumber}
-// Phonenumber_Directory.push(newEntry)
+let newEntry = { Name: inputName, PhoneNumber: inputNumber };
+Phonenumber_Directory.push(newEntry); // Henkilö lisätään tietokantaan
 
 // Haku funktio (Parametrit: taulukko + henkilön nimi), palauttaa puhelinnumeron jos löytyy
-function SearchDirectory(Directory, SearchName){
-    let PhoneNumber = null;
+function SearchDirectory(Directory, SearchName) {
+  let PhoneNumber = null;
 
-    Directory.forEach(element => {
-        if (SearchName === element.Name) 
-            PhoneNumber = element.PhoneNumber
-    });
+  Directory.forEach((element) => {
+    if (SearchName === element.Name) PhoneNumber = element.PhoneNumber;
+  });
 
-    return PhoneNumber;
+  return PhoneNumber;
 }
 
-// Haku funktion käyttö
-let Search = SearchDirectory(Phonenumber_Directory, inputName)
-console.log(Search)
+// Kysytään hakunimeä
+let SearchName = reader.question("Syötä haettavan henkilön nimi: ");
+let Search = SearchDirectory(Phonenumber_Directory, SearchName); // Haku
+
+// Tulostetaan haku tulos
+if (Search !== null) {
+  console.log("Henkilön " + SearchName + " puhelinnumero on: " + Search);
+} else {
+  console.log("Henkilöä nimeltä" + SearchName + "ei löytynyt.");
+}
